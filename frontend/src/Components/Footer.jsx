60ed6ecce1da294be1326d5ecf8376a372c7c908
Angely <angelycorahua2005@gmail.com>
2025-05-04 22:11:57 -0500

    refactor(frontend): update Navbar and Footer for Register navigation

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,11 +1,64 @@
-import "../styles/Footer.css";
+import { Link } from "react-router-dom"
+import "../styles/Footer.css"
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>© {new Date().getFullYear()} Cinerama. All rights reserved.</p>
-    </footer>
-  );
-};
+      <div className="footer-container">
+        <div className="footer-logo">
+          <Link to="/">CINERAMA</Link>
+        </div>
 
-export default Footer;
+        <div className="footer-columns">
+          <div className="footer-column">
+            <h3>Nosotros</h3>
+            <ul>
+              <li>
+                <Link to="/quienes-somos">Quienes somos</Link>
+              </li>
+              <li>
+                <Link to="/terminos">Términos y Condiciones</Link>
+              </li>
+              <li>
+                <Link to="/privacidad">Política de Privacidad</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer-column">
+            <h3>Atención al Cliente</h3>
+            <ul>
+              <li>
+                <Link to="/contacto">Contacto</Link>
+              </li>
+              <li>
+                <Link to="/preguntas-frecuentes">Preguntas Frecuentes</Link>
+              </li>
+              <li>
+                <Link to="/ayuda">Ayuda</Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer-column">
+            <h3>Síguenos</h3>
+            <div className="social-links">
+              <Link to="https://facebook.com" className="social-link">
+                Facebook
+              </Link>
+              <Link to="https://instagram.com" className="social-link">
+                Instagram
+              </Link>
+              <Link to="https://twitter.com" className="social-link">
+                Twitter
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright">© 2025 CINERAMA. Todos los derechos reservados</div>
+    </footer>
+  )
+}
+
+export default Footer
