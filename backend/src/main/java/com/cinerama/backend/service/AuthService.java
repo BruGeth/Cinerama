6dabed3eb9c90ca9aae44d9f0edadebdd603abf9
BruGeth <g15bru03@hotmail.com>
2025-05-13 11:46:13 -0500

    feat(auth): implement login logic in AuthController and services

--- backend/src/main/java/com/cinerama/backend/service/AuthService.java
+++ backend/src/main/java/com/cinerama/backend/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.cinerama.backend.service;
 
 import com.cinerama.backend.dto.LoginRequest;
+import com.cinerama.backend.dto.LoginResponse;
 import com.cinerama.backend.dto.RegisterRequest;
 import com.cinerama.backend.dto.VerificationRequest;
 import com.cinerama.backend.entity.User;
@@ -11,5 +12,6 @@ public interface AuthService {
 
     void verify(VerificationRequest request);
 
-    User login(LoginRequest request);
+    LoginResponse login(LoginRequest request);
+
 }
