9142c913b892a8248d601e5ff0d717712f695bb5
camilo-93 <camilothemiz09@gmail.com>
2025-05-13 13:44:18 -0500

    refactor(frontend): actualizacion del frontend

--- frontend/src/pages/Confectionery.jsx
+++ frontend/src/pages/Confectionery.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import '../styles/Confectionery.css';
 
 const Confectionery = () => {
-  const categorias = ["Todos", "Palomitas", "Gaseosas", "Dulces", "Combos"];
+  const categorias = ["Todos", "Palomitas", "Refrescos", "Dulces", "Combos"];
   const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("Todos");
 
   const productos = [
@@ -11,42 +11,42 @@ const Confectionery = () => {
       nombre: "Palomitas Grandes",
       descripcion: "Palomitas recién hechas, tamaño grande",
       precio: 16.90,
-      image: '/images/PromoSlider1.jpeg',
+      imagen: "https://p.turbosquid.com/ts-thumb/q1/WlHUvk/lQ/001/jpg/1625651436/600x600/fit_q87/d39477d2dde168a604144720e97ff678c657037f/001.jpg",
     },
     {
       categoria: "Palomitas",
       nombre: "Palomitas Medianas",
       descripcion: "Palomitas tamaño mediano",
       precio: 13.00,
-      imagen: "/img/palomitas-medianas.png",
+      imagen: "https://p.turbosquid.com/ts-thumb/q1/WlHUvk/iI/005/jpg/1625651396/1920x1080/fit_q99/c79b4c54b2c6d892dc8c955e89e9dde571a4599f/005.jpg",
     },
     {
-      categoria: "Gaseosas",
+      categoria: "Refrescos",
       nombre: "Refresco Grande",
       descripcion: "Refresco a elegir, tamaño grande",
       precio: 16.90,
-      imagen: "/img/refresco-grande.png",
+      imagen: "https://media.istockphoto.com/id/909938470/es/foto/comida-r%C3%A1pida-cola-bebe-copa-y-pajita.jpg?s=612x612&w=0&k=20&c=MV_j5gIoFgobJM_lTmhgDnpSt1g_eStiww1xrPFw8uA=  ",
     },
     {
-      categoria: "Gaseosas",
+      categoria: "Refrescos",
       nombre: "Refresco Mediano",
       descripcion: "Refresco a elegir, tamaño mediano",
       precio: 12.00,
-      imagen: "/img/refresco-mediano.png",
+      imagen: "https://media.istockphoto.com/id/956386632/es/foto/vaso-desechable-blanco-y-rojo-en-blanco-con-paja-imitan-para-arriba.jpg?s=612x612&w=0&k=20&c=de8VsML7qXdKd0HbxgMrejMfWCZpS7RpYTC5ApROuJI=",
     },
     {
       categoria: "Dulces",
       nombre: "Gomitas",
       descripcion: "Surtido de gomitas de frutas",
       precio: 7.00,
-      imagen: "/img/gomitas.png",
+      imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXPs2WEZERBz0Vh52PG_Ha30zwOr7j88n3Mg&s",
     },
     {
       categoria: "Combos",
       nombre: "Combo Individual",
       descripcion: "1 palomita mediana, 1 gaseosa mediana y 1 hot dog",
       precio: 37.00,
-      imagen: "/img/combo-individual.png",
+      imagen: "https://media.istockphoto.com/id/681903568/es/foto/palomitas-de-ma%C3%ADz-en-caja-con-cola.jpg?s=612x612&w=0&k=20&c=BFq5KXeKl9d29uciTycAu1hZYJg2ZF_3FM1avCq1vsk=",
     },
   ];
 
@@ -71,7 +71,7 @@ const Confectionery = () => {
     ))}
   </div>
 
-  <div className="product-grid">
+  <div className={`product-grid ${categoriaSeleccionada !== "Todos" ? "horizontal" : ""}`}>
     {productosFiltrados.map((prod, idx) => (
       <div key={idx} className="product-card">
         <img src={prod.imagen} alt={prod.nombre} className="product-image" />
