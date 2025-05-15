abd5c65a4c95765ce3e26863db5f1a49a3e141d8
Leo <leonsoles6@hotmail.com>
2025-05-15 12:30:46 -0500

    Updated AppRouter to include Cines page

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -15,6 +15,7 @@ import FestaRamaPage from '../pages/FestaRamaPage';
 import FestaRamaPackages from '../pages/FestaRamaPackages';
 import Profile from '../pages/Profile';
 import PrivateRoute from '../utils/PrivateRoute';
+import Cines from '../pages/Cines';
 
 const AppRouter = () => {
   return (
@@ -41,6 +42,7 @@ const AppRouter = () => {
           <Route path='/corporate' element={<Corporate/>}/>
           <Route path="/festaramapage" element={<FestaRamaPage />} />
           <Route path="/festarama/packages" element={<FestaRamaPackages />} />
+          <Route path="/cines" element={<Cines />} /> 
 
         </Routes>
       </main>
