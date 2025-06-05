1c575441b8ffed1ebe44a481ee78f98fc092fd32
camilo-93 <camilothemiz09@gmail.com>
2025-06-05 14:01:50 -0500

    refactor(Home): dynamic movie cards from MOVIES and add routing to detail view

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "../styles/Home.css";
 import Slider from "../Components/Slider";
 import PromoSlider from "../Components/PromoSlider";
-
+import { Link } from "react-router-dom";
+import { MOVIES } from "../Components/movies";
 const Home = () => {
 
-        /*{/* for video but no }*/
+  /*{/* for video but no }*/
 
 
   // const [showModal, setShowModal] = useState(false);
@@ -36,99 +37,24 @@ const Home = () => {
           <h2 className="home-subtitle">Películas en Cartelera</h2>
           <div className="home-card-container">
 
-            <div className="home-card">
-              <img
-                src="https://www.cinerama.com.pe/_admin/assets/images/peliculas/until_dawn-880447124-large.jpg"
-                alt="Misión"
-                className="home-card-image"
-              />
-              <div className="home-card-text">
-                <h3>UNTIL DAWN: NOCHE DE TERROR</h3>
-                <p>La historia sigue a un grupo de amigos que se ven atrapados en un ciclo de terror al intentar sobrevivir una noche mortífera...</p>
-                <button className="home-details-button">
-                  <i className="fa fa-info-circle"></i> Más detalles
-                </button>
-              </div>
-            </div>
 
-            <div className="home-card">
-              <img
-                src="https://www.cinerama.com.pe/_admin/assets/images/peliculas/star_wars_episode_iii_revenge_of_the_sith-477144354-large.jpg"
-                alt="Visión"
-                className="home-card-image"
-              />
-              <div className="home-card-text">
-                <h3>STAR WARS: EPISODIO III - LA VENGANZA DE LOS SITH</h3>
-                <p>La caída de Anakin Skywalker en el lado oscuro y el ascenso de Darth Sidious como emperador del primer Imperio Galáctico. También muestra la destrucción de la Orden Jedi, un plan cuidadosamente orquestado por Palpatine.</p>
-                <button className="home-details-button">
-                  <i className="fa fa-info-circle"></i> Más detalles
-                </button>
+            {MOVIES.map((movie) => (
+              <div key={movie.id} className="home-card">
+                    <img src={movie.image} alt={movie.title} className="home-card-image" />
+                <div className="home-card-text">
+                  <h3>{movie.title}</h3>
+                  <p>{movie.description}</p>
+                  <Link to={`/more-information/${movie.id}`}>
+                    <button className="home-details-button">
+                      <i className="fa fa-info-circle"></i> Más detalles
+                    </button>
+                  </Link>
+                </div>
               </div>
-            </div>
-
-            <div className="home-card">
-              <img
-                src="https://www.cinerama.com.pe/_admin/assets/images/peliculas/una%20pelicula%20de%20minecraft.jpg"
-                alt="Misión"
-                className="home-card-image"
-              />
-              <div className="home-card-text">
-                <h3>UNA PELICULA DE MINECRAFT</h3>
-                <p>Historia de cuatro inadaptados que, al ser arrastrados a un mundo de bloques, deben aprender a sobrevivir y trabajar juntos para regresar a su mundo</p>
-                <button className="home-details-button">
-                  <i className="fa fa-info-circle"></i> Más detalles
-                </button>
-              </div>
-            </div>
-
-            <div className="home-card">
-              <img
-                src="https://www.cinerama.com.pe/_admin/assets/images/peliculas/thunderbolts-poster-66f184dbe403e.jpg"
-                alt="Misión"
-                className="home-card-image"
-              />
-              <div className="home-card-text">
-                <h3>THUNDERBOLTS</h3>
-                <p>La viuda negra asesina Yelena (Florence Pugh) trabaja para Valentina Allegra de Fontaine. Su último encargo es volar un laboratorio en el sudeste asiático...</p>
-                <button className="home-details-button">
-                  <i className="fa fa-info-circle"></i> Más detalles
-                </button>
-              </div>
-            </div>
-
-            <div className="home-card">
-              <img
-                src="https://www.cinerama.com.pe/_admin/assets/images/peliculas/amateur.jpg"
-                alt="Misión"
-                className="home-card-image"
-              />
-              <div className="home-card-text">
-                <h3>THE AMATHEUR: OPERACIÓN VENGANZA</h3>
-                <p>Charlie Heller, un brillante pero retraído descodificador de la CIA, se ve obligado a tomar la justicia por su cuenta tras la muerte de su esposa.</p>
-                <button className="home-details-button">
-                  <i className="fa fa-info-circle"></i> Más detalles
-                </button>
-              </div>
-            </div>
-
-            <div className="home-card">
-              <img
-                src="https://www.cinerama.com.pe/_admin/assets/images/peliculas/until_dawn-880447124-large.jpg"
-                alt="Misión"
-                className="home-card-image"
-              />
-              <div className="home-card-text">
-                <h3>UNTIL DAWN: NOCHE DE TERROR</h3>
-                <p>Un grupo de amigos atrapados en un bucle temporal enfrentan a un asesino enmascarado. Cada vez que mueren, regresan al inicio de la noche.</p>
-                <button className="home-details-button">
-                  <i className="fa fa-info-circle"></i> Más detalles
-                </button>
-              </div>
-            </div>
+            ))}
 
           </div>
         </section>
-
         {/* for video but no */}
         {/* {showModal && (
           <div className="modal-overlay" onClick={closeModal}>
@@ -146,7 +72,6 @@ const Home = () => {
             </div>
           </div>
         )} */}
-
       </div>
     </section>
   );
