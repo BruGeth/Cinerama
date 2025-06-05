3ec7105a94afad963c8d16f479cb4cad1ab049c1
camilo-93 <camilothemiz09@gmail.com>
2025-06-05 14:08:05 -0500

    feat: add MoreInformation page with trailer, details, and buy button

---
+++ frontend/src/Components/MoreInformation.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { MOVIES } from "./movies"; // Make sure the path is correct
+import { FaTicketAlt } from "react-icons/fa";
+import "../styles/MoreInformation.css";
+
+const MoreInformation = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const movie = MOVIES.find((m) => m.id === parseInt(id));
+
+  const handleBuyClick = () => {
+    navigate(`/cartelera/${movie.id}`);
+  };
+
+  if (!movie) {
+    return (
+      <section className="more-info-wrapper">
+        <h2>Película no encontrada</h2>
+      </section>
+    );
+  }
+
+  return (
+    <section className="more-info-wrapper">
+      {/* Movie trailer */}
+      <div className="more-info-trailer">
+        <iframe
+          width="100%"
+          height="400"
+          src={movie.trailer}
+          title={`Tráiler ${movie.title}`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      {/* Movie details */}
+      <div className="more-info-content">
+        <img
+          className="more-info-image"
+          src={movie.image}
+          alt={movie.title}
+        />
+        <div className="more-info-text">
+          <h1>{movie.title}</h1>
+          <p>{movie.description}</p>
+          <button className="buy-button" onClick={handleBuyClick}>
+            <FaTicketAlt style={{ marginRight: '8px' }} />
+            Comprar
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MoreInformation;
