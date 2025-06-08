7a0722e3b8fbf91c7430d0e3bb7764a7148f0fe4
Camilo Asencio <camilothemiz09@gmail.com>
2025-06-07 22:19:48 -0500

    refactor: use lowercase 'movies' in Home component and update data source

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,25 +3,8 @@ import "../styles/Home.css";
 import Slider from "../Components/Slider";
 import PromoSlider from "../Components/PromoSlider";
 import { Link } from "react-router-dom";
-import { MOVIES } from "../Components/movies";
+import { movies } from "../Components/Movies";
 const Home = () => {
-
-  /*{/* for video but no }*/
-
-
-  // const [showModal, setShowModal] = useState(false);
-  // const [trailerUrl, setTrailerUrl] = useState('');
-
-  // const openTrailer = (url) => {
-  //   setTrailerUrl(url);
-  //   setShowModal(true);
-  // };
-
-  // const closeModal = () => {
-  //   setShowModal(false);
-  //   setTrailerUrl('');
-  // };
-
   return (
     <section className='home-wrapper'>
       <Slider />
@@ -38,7 +21,7 @@ const Home = () => {
           <div className="home-card-container">
 
 
-            {MOVIES.map((movie) => (
+            {movies.map((movie) => (
               <div key={movie.id} className="home-card">
                     <img src={movie.image} alt={movie.title} className="home-card-image" />
                 <div className="home-card-text">
@@ -55,23 +38,6 @@ const Home = () => {
 
           </div>
         </section>
-        {/* for video but no */}
-        {/* {showModal && (
-          <div className="modal-overlay" onClick={closeModal}>
-            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-              <iframe
-                width="560"
-                height="315"
-                src={trailerUrl}
-                title="Tráiler"
-                frameBorder="0"
-                allow="autoplay; encrypted-media"
-                allowFullScreen
-              ></iframe>
-              <button className="close-button" onClick={closeModal}>Cerrar</button>
-            </div>
-          </div>
-        )} */}
       </div>
     </section>
   );
