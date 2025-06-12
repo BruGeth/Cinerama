23a16a3684e6d55b2f6ac13af7bf9c73788c5b35
Angely <angelycorahua2005@gmail.com>
2025-06-12 12:21:50 -0500

    feat(backend): Enhance Seat entity with additional attributes for type, price, and relationships

--- backend/src/main/java/com/cinerama/backend/entity/Seat.java
+++ backend/src/main/java/com/cinerama/backend/entity/Seat.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "seat")
 @Data
@@ -20,7 +22,17 @@ public class Seat {
     private String seatNumber;
 
     @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
     private boolean available;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
 
     @ManyToOne
     @JoinColumn(name = "show_id", nullable = false)
