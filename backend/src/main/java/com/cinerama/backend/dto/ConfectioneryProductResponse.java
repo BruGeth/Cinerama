e8f2d1265fce07779deae6f39fbf1e50dcd0e452
BruGeth <g15bru03@hotmail.com>
2025-06-20 18:53:57 -0500

    feat: add ConfectioneryProductResponse DTO

---
+++ backend/src/main/java/com/cinerama/backend/dto/ConfectioneryProductResponse.java
@@ -0,0 +1,17 @@
+package com.cinerama.backend.dto;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ConfectioneryProductResponse {
+    private final Long id;
+    private final String name;
+    private final String description;
+    private final Double price;
+    private final String image;
+    private final ConfectioneryCategoryResponse category;
+}
