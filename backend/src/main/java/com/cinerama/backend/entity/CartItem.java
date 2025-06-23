a933a4b9f3efa3aafa5bce026176d8e8dae7a144
Leo <leonsoles6@hotmail.com>
2025-06-23 02:53:03 -0500

    chore(entity): create CartItem entity for future post-payment usage

---
+++ backend/src/main/java/com/cinerama/backend/entity/CartItem.java
@@ -0,0 +1,28 @@
+package com.cinerama.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties({"order"})
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    private int movieId;
+    private String title;
+    private int quantity;
+    private double price;
+}
