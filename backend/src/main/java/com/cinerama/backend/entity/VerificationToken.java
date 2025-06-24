db9b15c03ec88467e16391f2c12e2877564d6eb1
BruGeth <g15bru03@hotmail.com>
2025-06-24 13:07:31 -0500

    feat: add PasswordResetToken and VerificationToken entities for password recovery system

---
+++ backend/src/main/java/com/cinerama/backend/entity/VerificationToken.java
@@ -0,0 +1,21 @@
+package com.cinerama.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class
+VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+    @OneToOne
+    private User user;
+    private LocalDateTime expiryDate;
+}
