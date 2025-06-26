f6775ade17b741667bd517626251f555601f6818
Ashlee <jaeash26@gmail.com>
2025-06-26 13:57:28 -0500

    feat(backend): Create FestaRama repository to manage birthday events

---
+++ backend/src/main/java/com/cinerama/backend/repository/FestaRamaRepository.java
@@ -0,0 +1,7 @@
+package com.cinerama.backend.repository;
+
+import com.cinerama.backend.entity.FestaRama;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FestaRamaRepository extends JpaRepository<FestaRama, Long> {
+}
