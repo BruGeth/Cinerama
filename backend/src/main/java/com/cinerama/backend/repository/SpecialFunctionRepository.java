a366d26585acd1faa83986fdb6a8abd2fb12a7c5
Ashlee <jaeash26@gmail.com>
2025-06-27 22:54:01 -0500

    feat(backend): Create repository interface for the SpecialFunction entity

---
+++ backend/src/main/java/com/cinerama/backend/repository/SpecialFunctionRepository.java
@@ -0,0 +1,8 @@
+package com.cinerama.backend.repository;
+
+import com.cinerama.backend.entity.SpecialFunction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/* === Repository interface for SpecialFunction entity === */
+public interface SpecialFunctionRepository extends JpaRepository<SpecialFunction, Long>{
+}
