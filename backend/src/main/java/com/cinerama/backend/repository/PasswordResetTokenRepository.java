76cacae06d548d26641adf4ecc00b040f62919f7
BruGeth <g15bru03@hotmail.com>
2025-06-29 13:02:01 -0500

    feat: add method to retrieve all password reset tokens by user

--- backend/src/main/java/com/cinerama/backend/repository/PasswordResetTokenRepository.java
+++ backend/src/main/java/com/cinerama/backend/repository/PasswordResetTokenRepository.java
@@ -1,9 +1,13 @@
 package com.cinerama.backend.repository;
 
 import com.cinerama.backend.entity.PasswordResetToken;
+import com.cinerama.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
+    List<PasswordResetToken> findAllByUser(User user);
 }
