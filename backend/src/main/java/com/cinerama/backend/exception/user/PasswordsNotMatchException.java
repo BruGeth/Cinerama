118a84e66aef320bc0c6d28bcc555c2da88eab6f
BruGeth <g15bru03@hotmail.com>
2025-06-30 17:09:14 -0500

    feat: add documentation for user-related exception classes

--- backend/src/main/java/com/cinerama/backend/exception/user/PasswordsNotMatchException.java
+++ backend/src/main/java/com/cinerama/backend/exception/user/PasswordsNotMatchException.java
@@ -1,6 +1,15 @@
 package com.cinerama.backend.exception.user;
 
+/**
+ * Exception thrown when passwords do not match during user registration or password change.
+ * This exception is used to indicate that the provided passwords are not identical.
+ */
 public class PasswordsNotMatchException extends RuntimeException {
+    /**
+     * Creates a new instance of PasswordsNotMatchException with a default message.
+     *
+     * @param message the detail message explaining the reason for the exception.
+     */
     public PasswordsNotMatchException(String message) {
         super(message);
     }
--- backend/src/main/java/com/cinerama/backend/exception/user/UserNotFoundException.java
+++ backend/src/main/java/com/cinerama/backend/exception/user/UserNotFoundException.java
@@ -1,9 +1,21 @@
 package com.cinerama.backend.exception.user;
-
+/*
+ * Exception thrown when a user is not found in the system.
+ * This exception is used to indicate that the requested user does not exist in the database.
+ */
 public class UserNotFoundException extends RuntimeException {
+    /**
+     * Creates a new instance of UserNotFoundException with a default message.
+     * This constructor is used when no specific message is provided.
+     */
     public UserNotFoundException() {
         super("User not found");
     }
+    /**
+     * Creates a new instance of UserNotFoundException with a default message.
+     *
+     * @param message the detail message explaining the reason for the exception.
+     */
     public UserNotFoundException(String message) {
         super(message);
     }
