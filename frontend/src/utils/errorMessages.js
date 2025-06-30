40d0fcda144dba03ba292cf2751a7efe4545c6b8
BruGeth <g15bru03@hotmail.com>
2025-06-30 00:57:15 -0500

    feat: add error messages for user authentication and token validation

---
+++ frontend/src/utils/errorMessages.js
@@ -0,0 +1,10 @@
+const errorMessages = {
+  USER_NOT_FOUND: "Usuario no encontrado",
+  INVALID_TOKEN: "El código ingresado no es válido",
+  TOKEN_NOT_BELONG_USER: "El código no corresponde a este usuario",
+  EXPIRED_TOKEN: "El código ha expirado",
+  PASSWORDS_NOT_MATCH: "Las contraseñas no coinciden",
+  UNKNOWN_ERROR: "Ha ocurrido un error inesperado. Intenta nuevamente.",
+};
+
+export default errorMessages;
