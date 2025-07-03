3d42f0d669ccdd6bab97207adea8ff38b3efd383
Ashlee <jaeash26@gmail.com>
2025-07-03 15:41:32 -0500

    feat(backend): Add detailed documentation for Advertising, Event, FestaRama, and SpecialFunction entities

--- backend/src/main/java/com/cinerama/backend/entity/SpecialFunction.java
+++ backend/src/main/java/com/cinerama/backend/entity/SpecialFunction.java
@@ -5,6 +5,38 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 
+/**
+ * Represents the SpecialFunction entity in the Cinerama system.
+ *
+ * <p>This class maps to the `special_function` table in the database and defines
+ * the structure of the special function data, including details about the cinema,
+ * movie, attendees, contact information, and timestamps.</p>
+ *
+ * <h2>Fields:</h2>
+ * <ul>
+ *   <li><b>id:</b> Primary key for the SpecialFunction entity (auto-generated)</li>
+ *   <li><b>cinema:</b> Name of the cinema where the function takes place</li>
+ *   <li><b>movie:</b> Movie selected for the special function</li>
+ *   <li><b>date:</b> Date of the special function</li>
+ *   <li><b>time:</b> Time of the special function</li>
+ *   <li><b>attendees:</b> Number of attendees expected for the special function</li>
+ *   <li><b>requirements:</b> Additional requirements or notes for the special function</li>
+ *   <li><b>contactName:</b> Name of the contact person</li>
+ *   <li><b>contactEmail:</b> Email address of the contact person</li>
+ *   <li><b>contactPhone:</b> Phone number of the contact person</li>
+ *   <li><b>company:</b> Company organizing the special function</li>
+ *   <li><b>message:</b> Message or description of the special function</li>
+ *   <li><b>createdAt:</b> Timestamp indicating when the entity was created</li>
+ * </ul>
+ *
+ * <h2>Usage:</h2>
+ * <p>This entity is used to persist special function data in the database and is
+ * managed by the JPA repository.</p>
+ *
+ * @see jakarta.persistence.Entity
+ * @see org.hibernate.annotations.CreationTimestamp
+ */
+
 @Entity
 @Table(name = "special_function")
 @Getter
